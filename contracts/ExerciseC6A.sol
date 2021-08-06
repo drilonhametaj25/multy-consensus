1c1d358d291c900e1223635673764469dd2549d8
drilonhametaj25 <drilonhametaj25@gmail.com>
2021-08-07 01:14:49 +0200

    first

---
+++ contracts/ExerciseC6A.sol
@@ -0,0 +1,127 @@
+pragma solidity ^0.4.25;
+
+contract ExerciseC6A {
+
+    /********************************************************************************************/
+    /*                                       DATA VARIABLES                                     */
+    /********************************************************************************************/
+
+
+    struct UserProfile {
+        bool isRegistered;
+        bool isAdmin;
+    }
+
+    address private contractOwner;                  // Account used to deploy contract
+    mapping(address => UserProfile) userProfiles;   // Mapping for storing user profiles
+    bool private operational = true
+    uint constant M = 2
+    address[] multiCalls = new address[](0)
+
+
+    /********************************************************************************************/
+    /*                                       EVENT DEFINITIONS                                  */
+    /********************************************************************************************/
+
+    // No events
+
+    /**
+    * @dev Constructor
+    *      The deploying account becomes contractOwner
+    */
+    constructor
+                                (
+                                ) 
+                                public 
+    {
+        contractOwner = msg.sender;
+    }
+
+    /********************************************************************************************/
+    /*                                       FUNCTION MODIFIERS                                 */
+    /********************************************************************************************/
+
+    // Modifiers help avoid duplication of code. They are typically used to validate something
+    // before a function is allowed to be executed.
+
+    modifier requireIsOperational(){
+        return(operational, "Contract is currenty not operational");
+        _;
+    }
+
+    /**
+    * @dev Modifier that requires the "ContractOwner" account to be the function caller
+    */
+    modifier requireContractOwner()
+    {
+        require(msg.sender == contractOwner, "Caller is not contract owner");
+        _;
+    }
+
+    /********************************************************************************************/
+    /*                                       UTILITY FUNCTIONS                                  */
+    /********************************************************************************************/
+
+   /**
+    * @dev Check if a user is registered
+    *
+    * @return A bool that indicates if the user is registered
+    */   
+    function isUserRegistered
+                            (
+                                address account
+                            )
+                            external
+                            view
+                            returns(bool)
+    {
+        require(account != address(0), "'account' must be a valid address.");
+        return userProfiles[account].isRegistered;
+    }
+
+    function isOperational() public view returns(bool) {
+        return operational;
+    }
+
+    /********************************************************************************************/
+    /*                                     SMART CONTRACT FUNCTIONS                             */
+    /********************************************************************************************/
+
+    function registerUser
+                                (
+                                    address account,
+                                    bool isAdmin
+                                )
+                                external
+                                requireContractOwner
+    {
+        require(!userProfiles[account].isRegistered, "User is already registered.");
+
+        userProfiles[account] = UserProfile({
+                                                isRegistered: true,
+                                                isAdmin: isAdmin
+                                            });
+    }
+
+    function setOperatingStatus(bool mode) external (){
+        require(mode != operational, "New mode must be different from existing mode")
+        require(userProfiles[msg.sender].isAdmin, "Caller is not an admin");
+        bool isDuplicate = false;
+        for (uint c=0; c<multiCalls.length; c++){
+            if(multiCalls[c]==msg.sender){
+                isDuplicate = true;
+                break;
+            }
+        }
+
+        require(!isDuplicate, "Caller has already called this function");
+        multiCalls.push(msg.sender);
+
+        if(multiCalls.length >= M){
+            operational = mode
+            multiCalls = new address[](0)
+        }
+
+    }
+}
+
